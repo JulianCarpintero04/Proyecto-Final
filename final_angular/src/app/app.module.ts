4a144eb030ece3eee4ed506558a06886ca8da80b
Julian <juliancarpintero04@gmail.com>
2022-10-12 17:50:20 -0300

    Maquetando

--- final_angular/src/app/app.module.ts
+++ final_angular/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { LoginItemComponent } from './components/login-item/login-item.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { AcercamioComponent } from './components/acercamio/acercamio.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { PortfolioService } from './servicio/portfolio.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HabilidadesComponent,
+    ContactoComponent,
+    LoginItemComponent,
+    EncabezadoComponent,
+    AcercamioComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ final_angular/src/app/components/acercamio/acercamio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acercamio',
+  templateUrl: './acercamio.component.html',
+  styleUrls: ['./acercamio.component.css']
+})
+export class AcercamioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
