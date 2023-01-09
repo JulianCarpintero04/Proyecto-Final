0b0b2cf83a6dc1925c685cef5ae695e7b3f2be43
Julian <juliancarpintero04@gmail.com>
2023-01-09 10:38:23 -0300

    Ultimos commits

--- final_angular/src/app/app.module.ts
+++ final_angular/src/app/app.module.ts
@@ -6,14 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
-import { LoginItemComponent } from './components/login-item/login-item.component';
 import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 import { AcercamioComponent } from './components/acercamio/acercamio.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { PortfolioService } from './servicio/portfolio.service';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InterceptorService } from './servicio/interceptor.service';
 
 @NgModule({
   declarations: [
@@ -21,19 +24,23 @@ import {HttpClientModule} from '@angular/common/http';
     HeaderComponent,
     HabilidadesComponent,
     ContactoComponent,
-    LoginItemComponent,
     EncabezadoComponent,
     AcercamioComponent,
     ExperienciaComponent,
     EducacionComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    IniciarSesionComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [PortfolioService,
+    { provide:HTTP_INTERCEPTORS, useClass:InterceptorService, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ final_angular/src/app/components/iniciar-sesion/iniciar-sesion.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacionService } from 'src/app/servicio/autenticacion.service';
+
+@Component({
+  selector: 'app-iniciar-sesion',
+  templateUrl: './iniciar-sesion.component.html',
+  styleUrls: ['./iniciar-sesion.component.css']
+})
+export class IniciarSesionComponent implements OnInit {
+  form:FormGroup;
+  constructor(private formBuilder:FormBuilder, private autenticacionService:AutenticacionService, private ruta:Router) { 
+    this.form=this.formBuilder.group(
+      {
+        email:['',[Validators.required,Validators.email]],
+        contrasena:['',[Validators.required,Validators.minLength(8)]]
+      }
+    )
+  }
+
+  ngOnInit(): void {
+  }
+
+  get Email(){
+    return this.form.get('email');
+  }
+
+  get Contrasena(){
+    return this.form.get('contrasena');
+  }
+
+  onEnviar(event:Event){
+    event.preventDefault;
+    this.autenticacionService.IniciarSesion(this.form.value).subscribe(data=>{
+      console.log("DATA:" + JSON.stringify(data));
+      this.ruta.navigate(['/portfolio']);
+    })
+  }
+}
